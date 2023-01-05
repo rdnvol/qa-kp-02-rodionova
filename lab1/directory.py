d1104f29fa0658ab84813465d1113e3705e48680
rdnvol <rdnv.ol20@gmail.com>
2023-01-06 01:54:24 +0200

    directory.py is updated and tests for it are created

--- lab1/directory.py
+++ lab1/directory.py
@@ -1,14 +1,18 @@
 class Directory:
-    def __init__(self, dirName, maxElNumber = 0, parent = None) -> None:
+    def __init__(self, dirName, content = [], maxElNumber = 0, parent = None) -> None:
         self.name = dirName
+        self.content = content
+        self.maxElementsNumber = maxElNumber
+        self.parent = parent
         pass
 
     def __delete_dir__(self) -> None:
-        self is None
-        pass
+        del self
+
 
     def __get_content_list__(self) -> None:
-        pass
+        return len(self.content)
 
-    def __move_content_to_another_dir__(self) -> None:
-        pass
+    def __move_content_to_another_dir__(self, item, path) -> None:
+        self.content = list(filter(lambda x: item in x, self.content))
+        return path.content.append(item)
--- lab1/test_fs.py
+++ lab1/test_fs.py
@@ -1,5 +1,9 @@
 import pytest
 from directory import Directory
+from binary_file import BinaryFile
+from log_text_file import LogTextFile
+from buffer_file import BufferFile
+
 
 class TestDirectory:
     mainDirectory = Directory('main')
@@ -15,16 +19,92 @@ class TestDirectory:
         
     def test_directory_delete(self):
         directory = Directory('main')
-        directory.__delete_dir__(self)
-        assert directory != None
+
+        assert directory.__delete_dir__() is None
 
     def test_list_of_directory_content(self):
-        pass
+        directory = Directory('main')
+        
+        assert directory.__get_content_list__() == 0
 
-    def test_directory_init(self):
-        pass
+        directory.content = ['file1', Directory('1')]
+        assert directory.__get_content_list__() is not None
+
+    def test_directory_move(self):
+        directory = Directory('for_move')
+        newDir = Directory('newDir', maxElNumber = 5)
+        directory.__move_content_to_another_dir__('file1', newDir)
+        assert len(newDir.content) != 0
 
 
 
+# class TestBinaryFile:
+#     binaryFile = BinaryFile('binary_file', 'any content', )
+
+#     def test_binary_file_init(self):
+#         name = 'binary_file1'
+#         binaryFile = BinaryFile(name)
+
+#         assert binaryFile.name == name
+
+        
+#     def test_directory_delete(self):
+#         directory = Directory('main')
+#         directory.__delete_dir__(self)
+#         assert directory != None
+
+#     def test_list_of_directory_content(self):
+#         pass
+
+#     def test_directory_init(self):
+#         pass
+
+
+# class TestLogTextFile:
+#     mainDirectory = Directory('main')
+#     main = ""
+
+#     def test_directory_init(self):
+#         name = 'new_name'
+#         maxElementsNumber = 5
+#         directory = Directory(name, maxElementsNumber)
+
+#         assert directory.name == name
+
+        
+#     def test_directory_delete(self):
+#         directory = Directory('main')
+#         directory.__delete_dir__(self)
+#         assert directory != None
+
+#     def test_list_of_directory_content(self):
+#         pass
+
+#     def test_directory_init(self):
+#         pass
+
+
+# class TestBufferFile:
+    # mainDirectory = Directory('main')
+    # main = ""
+
+    # def test_directory_init(self):
+    #     name = 'new_name'
+    #     maxElementsNumber = 5
+    #     directory = Directory(name, maxElementsNumber)
+
+    #     assert directory.name == name
+
+        
+    # def test_directory_delete(self):
+    #     directory = Directory('main')
+    #     directory.__delete_dir__(self)
+    #     assert directory != None
+
+    # def test_list_of_directory_content(self):
+    #     pass
+
+    # def test_directory_init(self):
+    #     pass
 
 
