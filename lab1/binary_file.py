09ed45569ea0d17a6bb591a0955be63eeb2bf97a
rdnvol <rdnv.ol20@gmail.com>
2023-01-07 00:31:06 +0200

    binary_file.py was updated, the TestBinaryFile class was created in the test_fs file

--- lab1/binary_file.py
+++ lab1/binary_file.py
@@ -3,17 +3,25 @@ from directory import Directory
 class BinaryFile:
     def __init__(self, fileName, parent = None) -> None:
         self.name = fileName
-        self.directory = parent
+        self.parent = parent
+        self.content = ""
         if type(parent) is Directory:
-            parent.content.append(self.name)
-        pass
+            parent.content.append(self)
 
-    def __delete_file__(self) -> None:
+    def __delete_binory_file__(self) -> None:
+        self.parent.content.pop(self.parent.content.index(self))
         del self
 
-    def __move_file__(self, directory, ) -> None:
+    def __move_binory_file__(self, path) -> None:
+        if type(path) != Directory or None:
+            raise OverflowError('incorrect path' + path)
 
-        pass
+        if len(path.content) + 1 > path.maxElementsNumber:
+            raise OverflowError('there are no space for new file in directory: ' + path.name)
 
-    def __read_file__(self) -> None:
-        pass
+        self.parent.content.pop(self.parent.content.index(self))
+        path.content.append(self)
+        self.parent = path
+
+    def __read_binory_file__(self) -> None:
+        return self.content
--- lab1/test_fs.py
+++ lab1/test_fs.py
@@ -22,6 +22,7 @@ class TestDirectory:
         directory = Directory('main')
 
         assert directory.__delete_dir__() is None
+        print(directory)
 
     def test_list_of_directory_content(self):
         directory = Directory('main')
@@ -47,6 +48,7 @@ class TestDirectory:
         
         directory.__move_content_to_another_dir__(file_for_move, dir_fo_move)
         assert len(dir_fo_move.content) == 1
+        assert file_for_move.parent == dir_fo_move
 
         with pytest.raises(OverflowError):
             directory.__move_content_to_another_dir__(file2, dir_fo_move)
@@ -56,7 +58,35 @@ class TestDirectory:
             directory.__move_content_to_another_dir__(file_for_move, dir_for_move2)
 
         
+class TestBinaryFile:
+    directory1 = Directory('dir1', 2)
+    directory2 = Directory('dir2', 2)
+    def test_binary_file_init(self):
+        binaryFile = BinaryFile('binary_file1', self.directory1)
 
+        assert len(self.directory1.content) != 0
+        
+    def test_binary_file_delete(self):
+        directory = Directory('dir1')
+        binaryFile = BinaryFile('binary_file', directory)
+
+        binaryFile.__delete_binory_file__()
+        assert any(item in directory.content for item in directory.content) is False
+
+    def test_move_binary_file(self):
+        directory = Directory('dir1')
+        binaryFile = BinaryFile('binary_file1', directory)
+
+        binaryFile.__move_binory_file__(self.directory2)
+        assert any(binaryFile in self.directory2.content for binaryFile in self.directory2.content) is True
+
+
+    def test_read_binary_file(self):
+        content = 'binary file content'
+        binaryFile = BinaryFile('binary_file1')
+        binaryFile.content = content
+
+        assert type(binaryFile.__read_binory_file__()) is str
 
 
 # class TestLogTextFile:
